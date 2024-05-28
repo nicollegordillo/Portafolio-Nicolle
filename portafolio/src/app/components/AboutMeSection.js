0e3258a9d5ce00d90d0eae3914df4c86b1002219
nicollegordillo <nickygordillo912@gmail.com>
2024-05-28 15:27:00 -0600

    Fix: Technologies page

--- portafolio/src/app/components/AboutMeSection.js
+++ portafolio/src/app/components/AboutMeSection.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {interest, school} from '../images';
 
 const AboutMeSection = () => {
   return (
     
-    <div id="about-me" className="about-me-section speech-bubble"> {/* Apply both classes */}
-      <h2>Sobre mí</h2>
-      <p>
+    <div id="about-me" className="cover about-me-section speech-bubble"> {/* Apply both classes */}
+      <h2 style={{fontsize: '4em'}} >Sobre mí</h2>
+      <div className="imagebox">
+        <img src={school} alt="image" style={{ width: '5%', height: 'auto' }} />
+      </div>
+      <p style={{fontsize: '4em'}}>
         Hola!! Soy Nicolle Gordillo. Actualmente estudio Ingeniería en Ciencias de la Computación y Tecnologías de la Información en la Universidad del Valle de Guatemala. También tengo un Bachillerato en Ciencias y Letras con orientación en Computación.
       </p>
+      <div className="imagebox">
+        <img src={interest} alt="image" style={{ width: '5%', height: 'auto' }} />
+      </div>
       <p>
         Más allá de mis actividades académicas, Me apasiona la tecnología, los videojuegos y el manga. Hablo inglés fluido y japonés intermedio.
       </p>
--- portafolio/src/app/components/TechnologiesSection.js
+++ portafolio/src/app/components/TechnologiesSection.js
@@ -1,34 +1,38 @@
 import React from 'react';
+import {c,java,javas,kotlin,python,html,css,react,node,next,mysql,postgres,oracle} from '../images';
 
 const TechnologiesSection = ({id}) => {
   return (
-    <div id={id} className="technologies-section">
-      <h2>Technologies & Languages</h2>
-      <div className="section">
-        <h3>Programming Languages</h3>
-        <div className="icons">
-          {/* Add programming language icons here with respective URLs */}
-          <a href="/javascript"><img src="javascript-icon.png" alt="JavaScript" /></a>
-          <a href="/python"><img src="python-icon.png" alt="Python" /></a>
-          {/* Add more icons for programming languages as needed */}
+    <div id={id} className="cover technologies-section">
+      <h2>Tecnologías y Lenguajes</h2>
+      <div className="sections-container">
+        <div className="section">
+          <h3>Programming Languages</h3>
+          <div className="icons">
+            <a href="https://dotnet.microsoft.com/es-es/languages/csharp"><img src={c} alt="c#" style={{width:'8em', height:'auto'}}/></a>
+            <a href="https://www.java.com/es/"><img src={java} alt="java" style={{ width: '5em', height: 'auto' }}/></a>
+            <a href="https://kotlinlang.org/"><img src={kotlin} alt="kotlin"  style={{ width: '8em', height: 'auto' }}/></a>
+            <a href="https://www.python.org/"><img src={python} alt="python"  style={{ width: '5em', height: 'auto' }}/></a>
+          </div>
         </div>
-      </div>
-      <div className="section">
-        <h3>Databases</h3>
-        <div className="icons">
-          {/* Add database icons here with respective URLs */}
-          <a href="/mongodb"><img src="mongodb-icon.png" alt="MongoDB" /></a>
-          <a href="/mysql"><img src="mysql-icon.png" alt="MySQL" /></a>
-          {/* Add more icons for databases as needed */}
+        <div className="section">
+          <h3>Databases</h3>
+          <div className="icons">
+            <a href="https://www.mysql.com/"><img src={mysql} alt="mysql"  style={{ width: '8em', height: 'auto' }}/></a>
+            <a href="https://www.postgresql.org/"><img src={postgres} alt="postgresql"  style={{ width: '8em', height: 'auto' }}/></a>
+            <a href="https://www.oracle.com/"><img src={oracle} alt="oracle"  style={{ width: '8em', height: 'auto' }}/></a>
+          </div>
         </div>
-      </div>
-      <div className="section">
-        <h3>Web Development</h3>
-        <div className="icons">
-          {/* Add web development icons here with respective URLs */}
-          <a href="/html"><img src="html-icon.png" alt="HTML" /></a>
-          <a href="/css"><img src="css-icon.png" alt="CSS" /></a>
-          {/* Add more icons for web development as needed */}
+        <div className="section">
+          <h3>Web Development</h3>
+          <div className="icons">
+            <a href="https://developer.mozilla.org/es/docs/Web/HTML"><img src={html} alt="html"  style={{ width: '4em', height: 'auto' }}/></a>
+            <a href="https://developer.mozilla.org/es/docs/Web/CSS"><img src={css} alt="css"  style={{ width: '4em', height: 'auto' }}/></a>
+            <a href="https://www.javascript.com/"><img src={javas} alt="javascript"  style={{ width: '9em', height: 'auto' }}/></a>
+            <a href="https://nextjs.org/"><img src={next} alt="next"  style={{ width: '5em', height: 'auto' }}/></a>
+            <a href="https://nodejs.org/en"><img src={node} alt="node js"  style={{ width: '5em', height: 'auto' }}/></a>
+            <a href="https://es.react.dev/"><img src={react} alt="react"  style={{ width: '10em', height: 'auto' }}/></a>
+          </div>
         </div>
       </div>
     </div>
